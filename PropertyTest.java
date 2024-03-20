8e2fcad9325141153119943a5599bb3d978fe7c0
rodri <202100213@alunos.ips.pt>
2024-03-20 08:45:26 +0000

    lvl 1

---
+++ PropertyTest.java
@@ -0,0 +1,48 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+/**
+ * The test class PropertyTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class PropertyTest
+{
+    /**
+     * Default constructor for test class PropertyTest
+     */
+    public PropertyTest()
+    {
+    }
+
+    @Test
+    public void testConstructor(){
+        Property propety1= new Property("descricao",120000.20);
+        assertEquals("descricao",propety1.getDescription());
+    }
+    
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+}
